f49c7a3e0cb7469e941545c0217a2459f2ba5b45
VadymStriuthenko <vadym.striuchenko@auk.edu.ua>
2026-09-21 09:16:08 +0300

    1

--- src/part1_arrays/part1.java
+++ src/part1_arrays/part1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Part1 {
+public class part1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
--- src/part2_wrappers/part2.java
+++ src/part2_wrappers/part2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Part2 {
+public class part2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
